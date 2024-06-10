868d3ce1f018b29e324325a5ed83b88321c7f5e8
patel-nancy <nancy.patel@wustl.edu>
2024-06-10 13:27:06 -0400

    moving around directories

---
+++ tests/Task 3.py
@@ -0,0 +1,13 @@
+OK_FORMAT = True
+
+test = {   'name': 'Task 3',
+    'points': 10,
+    'suites': [   {   'cases': [   {   'code': ">>> '0' in counts and counts['0'] == 468\nTrue",
+                                       'failure_message': 'EXPECTED simpleCircuit TO BE RUN 468 TIMES',
+                                       'hidden': False,
+                                       'locked': False,
+                                       'points': 10}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
